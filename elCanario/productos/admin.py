75c94221b4ceef01b3917a9e83ef773256dc7b85
DarthKenar <federico_vega22@hotmail.com>
2023-02-21 15:22:45 -0300

    🛠 Refactored admin.py little small change
     Changes to be committed:
    	modified:   elCanario/productos/__pycache__/admin.cpython-311.pyc
    	modified:   elCanario/productos/admin.py

--- elCanario/productos/admin.py
+++ elCanario/productos/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 
-admin.site.register(models.Stock)
+class StockAdmin(admin.ModelAdmin):
+    list_display = ("nombre","tipo","color","material","talle","precio_compra","incremento","precio_venta")
+    search_fields = ("nombre","tipo")
+    list_filter = ["tipo"]
+    ordering = ['nombre']
+
+admin.site.register(models.Stock, StockAdmin)
