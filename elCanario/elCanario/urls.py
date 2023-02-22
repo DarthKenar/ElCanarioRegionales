eda5fb9668a802969874f33ce0ceb00a01d52e17
DarthKenar <federico_vega22@hotmail.com>
2023-02-22 10:53:08 -0300

    🛠 refactored urls.py - deleted productos/urls.py
    i placed urls only in elCanario/urls.py to concentrate the urls in one files
    
    i think is better becouse its more readable
    On branch master
    Changes to be committed:
    	modified:   elCanario/urls.py
    	deleted:    productos/urls.py

--- elCanario/elCanario/urls.py
+++ elCanario/elCanario/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path
+from productos import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('productos/', include('productos.urls'))
+    path('productos/', views.productos),
+    path('productos/buscar/', views.buscar),
 ]
