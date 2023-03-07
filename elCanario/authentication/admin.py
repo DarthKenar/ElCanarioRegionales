8137cc3dbdc418abb35cb228bafc19b05f1bdb27
DarthKenar <federico_vega22@hotmail.com>
2023-03-07 00:10:02 -0300

    🔨 Refactor Code:  only change the lenguage (admin)
    variables now have English names
    
    On branch master
    Changes to be committed:
    	modified:   authentication/admin.py
    	deleted:    productos/admin.py

--- elCanario/authentication/admin.py
+++ elCanario/authentication/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import User
 # Register your models here.
 class AdminUser(admin.ModelAdmin):
-    list_display = ['nombre','correo','contrasenia']
+    list_display = ['username','email','phone_number','password']
 
 admin.site.register(User, AdminUser)
