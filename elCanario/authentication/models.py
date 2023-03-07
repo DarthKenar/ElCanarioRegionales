4b0a6379f90700c395061c8bc0c1690d3af10d88
DarthKenar <federico_vega22@hotmail.com>
2023-03-07 00:07:04 -0300

    🔨 Refactor Code:  only change the lenguage (models)
    variables now have English names
    
    On branch master
    Changes to be committed:
    	modified:   authentication/models.py
    	deleted:    productos/models.py

--- elCanario/authentication/models.py
+++ elCanario/authentication/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    nombre = models.CharField(verbose_name="Nombre", max_length=32)
-    correo = models.EmailField(verbose_name="Correo Electrónico", max_length=32)
-    telefono = models.CharField(verbose_name="numero de teléfono", max_length=32)
-    contrasenia = models.CharField(verbose_name="Password", max_length=32)
+    username = models.CharField(verbose_name="Nombre", max_length=32)
+    email = models.EmailField(verbose_name="Correo Electrónico", max_length=32)
+    phone_number = models.CharField(verbose_name="numero de teléfono", max_length=32)
+    password = models.CharField(verbose_name="Password", max_length=32)
 
 
 
