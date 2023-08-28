54ac7f98277f4d17b1d63b655366bbbd026252ec
Federico Vega - HP <federico_Vega22@hotmail.com>
2023-08-28 15:18:40 -0300

    💩 Needs Improvement:  merge broken..

--- elCanario/elCanario/urls.py
+++ elCanario/elCanario/urls.py
@@ -43,14 +43,9 @@ urlpatterns = [
     ## ARTICLES SECTIONS
     path('articles', include(articles_urlpatterns)),
 
-
     #CUSTOMERS
-    path('customers', articles_views.customers, name="customers"),
-    path('customers_create', articles_views.customers_create, name="customers_create"),
-    path('customers_read_datatype', articles_views.customers_read_datatype, name="customers_read_datatype"),
-    path('customers_read_data', articles_views.customers_read_data, name="customers_read_data"),
-    # path('customers_update/<int:id>', articles_views.customers_read_data_update, name="customers_update"),
-    # path('customers_delete/<int:id>', articles_views.customers_read_data_delete, name="customers_delete"),
+    path('customers', include(customers_urlpatterns)),
+
 ]
 htmx_urlpatterns = [
 #     #ARTICLES SETCIONS
