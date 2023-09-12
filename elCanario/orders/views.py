481135ec84ae76fb167fe95eb213c9c48d77126d
DarthKenar <federico_vega22@hotmail.com>
2023-09-12 15:34:47 -0300

    �✨ New Feature: Now, its possible delete orders

--- elCanario/orders/views.py
+++ elCanario/orders/views.py
@@ -147,7 +147,7 @@ def order_delete(request:object, pk:int)-> HttpResponse:
         context["order_delete_answer"] = f"The selected order could not be deleted because it does not exist. Contact support."
         return render_login_required(request, template, context)
     else:
-        context["customer_delete_answer"] = f"Order {order.pk} for {order.customer_id} has been eliminated"
+        context["order_delete_answer"] = f"Order {order.pk} for {order.customer_id} has been eliminated"
         message = MessageLog(info=f"Order {order.pk} for {order.customer_id} has been eliminated")
         message.save()
         order.delete()
