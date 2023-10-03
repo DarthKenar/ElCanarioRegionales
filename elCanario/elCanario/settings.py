829dfe697f2dc826b99c63525262c5abfff082c3
DarthKenar <federico_vega22@hotmail.com>
2023-10-03 04:33:22 -0300

    Google account config

--- elCanario/elCanario/settings.py
+++ elCanario/elCanario/settings.py
@@ -165,16 +165,18 @@ DATABASES = {
 # Provider specific settings
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
-        # For each OAuth based provider, either add a ``SocialApp``
-        # (``socialaccount`` app) containing the required client
-        # credentials, or list them here:
-        'APP': {
-            'client_id': '123',
-            'secret': '456',
-            'key': ''
-        }
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        },
+        'OAUTH_PKCE_ENABLED': True,
     }
 }
+
+SITE_ID = 1
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
