620a1bc84583480b88c46c8fc700e8adedf90ed6
Federico Vega <federico_vega22@hotmail.com>
2023-10-25 22:21:10 -0300

    Translate added! Now Translate works right

--- elCanario/elCanario/settings.py
+++ elCanario/elCanario/settings.py
@@ -85,6 +85,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
+    'django.middleware.locale.LocaleMiddleware'
 ]
 
 ROOT_URLCONF = 'elCanario.urls'
