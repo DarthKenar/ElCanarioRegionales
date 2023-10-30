d0b5eca716abd662645bb5d7ac81c90956ca2d51
Federico Vega <federico_vega22@hotmail.com>
2023-10-30 18:59:17 -0300

    👌 Code review :SETTINGS MODULEU

--- elCanario/settings/views.py
+++ elCanario/settings/views.py
@@ -6,4 +6,10 @@ from elCanario.utils import render_login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from PIL import Image
 class GeneralSettingsView(LoginRequiredMixin, TemplateView):
+    """Redirects to the Configuration or My Account view
+
+    Args:
+        LoginRequiredMixin: Allow access to the view for registered users only.
+        TemplateView: TemplateView Generic
+    """
     template_name = 'settings.html'
